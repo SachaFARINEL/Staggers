eb80ea4c31ee75522e3997a661afa114ef6c9473
Sacha FARINEL <farinel.sacha@tutanota.com>
2022-04-12 22:36:26 +0200

    Modèle ChoiceBox

--- src/main/java/ihm/inscriptionController.java
+++ src/main/java/ihm/inscriptionController.java
@@ -1,5 +1,22 @@
 package ihm;
 
-public class inscriptionController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class inscriptionController implements Initializable {
+
+    private final String[] arraySexe = {"M", "Mme", "Autre"};
+
+    @FXML
+    private ChoiceBox<String> sexe;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        sexe.getItems().addAll(arraySexe);
+
+    }
 }
