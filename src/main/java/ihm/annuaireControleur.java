aee3561693cb07f7796f85b0ba1a1fa1dca52b0e
Clement B <clement.bollore@gmail.com>
2022-05-13 11:42:30 +0200

    commit création annuaire

---
+++ src/main/java/ihm/annuaireControleur.java
@@ -0,0 +1,54 @@
+package ihm;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+public class annuaireControleur {
+
+    @FXML
+    private Label deco;
+
+    @FXML
+    private Label fil_actualite;
+
+    @FXML
+    private Label profil;
+
+    @FXML
+    private Label annuaire;
+
+    @FXML
+    private Label conseils;
+
+    @FXML
+    private Button details;
+
+    @FXML
+    void versActualite(MouseEvent event) throws IOException {
+        Main main = new Main();
+        main.nextScene("actualite-view.fxml");
+    }
+    @FXML
+    void versAnnuaire(MouseEvent event) throws IOException {
+        Main main = new Main();
+        main.nextScene("annuaire-view.fxml");
+    }
+    @FXML
+    void versConseil(MouseEvent event) throws IOException {
+        Main main = new Main();
+        main.nextScene("conseil-view.fxml");
+    }
+    @FXML
+    void versProfil(MouseEvent event) throws IOException {
+        Main main = new Main();
+        main.nextScene("profil-view.fxml");
+    }
+    @FXML
+    void deconnexion(MouseEvent event) throws IOException {
+        Main main = new Main();
+        main.nextScene("loggin-view.fxml");
+    }
+}
